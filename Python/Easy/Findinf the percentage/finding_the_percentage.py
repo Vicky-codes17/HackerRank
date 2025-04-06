226efba5fc05a517c270fb2f349284bed3218380
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-04-06 13:13:00 +0530

    modified and new files

--- Python/Easy/Findinf the percentage/finding_the_percentage.py
+++ Python/Easy/Findinf the percentage/finding_the_percentage.py
@@ -7,4 +7,14 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = input()
     result = student_marks[query_name] / student_marks[query_name]
-    print(format(result), ".2f")
+
+    print(format(result), "%.2f")
+
+
+
+
+#     for j in student_marks[query_name]:
+#         total = total + j
+
+# per = total/len(student_marks[query_name])
+# print("%.2f" % per)
